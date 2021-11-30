1a8fd898dd9a01af9b53d224049f85b67a82736b
krethh <krecik.kret@gmail.com>
2021-11-30 22:13:33 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import days.day1.Day1;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Day1.solve(Paths.get("C:\\ws\\advent-of-code-2021\\src\\resources\\day1.txt"));
+    }
+}
---
+++ src/days/day1/Day1.kt
@@ -0,0 +1,12 @@
+package days.day1
+
+import java.io.IOException
+import java.nio.file.Path
+
+object Day1 {
+    @JvmStatic
+    @Throws(IOException::class)
+    fun solve(input: Path?) {
+
+    }
+}
