c855f5d0724575b746b0f81175b5af90dde44f7b
krethh <krecik.kret@gmail.com>
2021-12-01 08:12:35 +0100

    First day

--- src/days/day1/Day1.kt
+++ src/days/day1/Day1.kt
@@ -1,12 +1,34 @@
 package days.day1
 
 import java.io.IOException
+import java.nio.file.Files
 import java.nio.file.Path
 
 object Day1 {
     @JvmStatic
     @Throws(IOException::class)
     fun solve(input: Path?) {
+        val items = Files.readAllLines(input).map { it.toInt() }
 
+        var counter = 0;
+        for (i in 1 until items.size) {
+            if (items[i] > items[i - 1]) {
+                counter ++;
+            }
+        }
+
+        println(counter)
+
+        counter = 0;
+        var lastWindow = items[0] + items[1] + items[2];
+        for (i in 3 until items.size) {
+            val thisWindow = items[i] + items[i - 1] + items[i - 2]
+            if (thisWindow > lastWindow) {
+                counter++;
+            }
+            lastWindow = thisWindow;
+        }
+
+        println(counter)
     }
 }
