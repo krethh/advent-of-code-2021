5689c9be6e1a24bc95b804605e70f99082f8f058
krethh <krecik.kret@gmail.com>
2021-12-07 08:32:55 +0100

    Day 7 refactor

--- src/days/day7/Day7.kt
+++ src/days/day7/Day7.kt
@@ -11,38 +11,19 @@ object Day7 {
     fun solve(input: Path?) {
         val items = Files.readAllLines(input)[0].split(",").map { it.toInt() }
 
-        solve(items, partOne = true)
-        solve(items, false)
+        println(solve(items, partOne = true))
+        println(solve(items, partOne = false))
     }
 
-    private fun solve(items: List<Int>, partOne: Boolean) {
-        var minSum = 0
-        for (item in 0..items.maxOrNull()!!) {
-            var sum = 0
-            items.forEach { sum += calculateDistance(item, it, partOne) }
-            if (minSum == 0) {
-                minSum = sum
-            }
-            if (sum < minSum) {
-                minSum = sum
-            }
-        }
-
-        println(minSum)
+    private fun solve(items: List<Int>, partOne: Boolean): Int {
+        return IntRange(0, items.maxOrNull()!!).minOfOrNull { target ->
+            items.sumOf { innerItem -> calculateDistance(innerItem, target, partOne) }
+        }!!
     }
 
     private fun calculateDistance(item: Int, target: Int, partOne: Boolean): Int {
-        if (partOne) {
-            return Math.abs(item - target)
-        }
-
         val distance = Math.abs(item - target)
-        var cost = 0
-        for (i in 0..distance) {
-            cost += i
-        }
-        return cost
+        return if (partOne) distance else IntRange(0, distance).sum()
     }
-
 
 }
