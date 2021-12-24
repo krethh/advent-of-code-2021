982c002fe022956262df4d1d52144138b3f179fe
krethh <krecik.kret@gmail.com>
2021-12-24 20:20:21 +0100

    Day 24

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ import days.day20.Day20;
 import days.day21.Day21;
 import days.day22.Day22;
 import days.day23.Day23;
+import days.day24.Day24;
 import days.day3.Day3;
 import days.day4.Day4;
 import days.day5.Day5;
@@ -50,8 +51,8 @@ public class Main {
 //        Day20.solve(Paths.get("C:\\ws\\advent-of-code-2021\\src\\resources\\day20.txt"));
 //        Day21.solve(Paths.get("C:\\ws\\advent-of-code-2021\\src\\resources\\day21.txt"));
 //        Day22.solve(Paths.get("C:\\ws\\advent-of-code-2021\\src\\resources\\day22.txt"));
-        Day23.solve(Paths.get("C:\\ws\\advent-of-code-2021\\src\\resources\\day23.txt"));
-//        Day24.solve(Paths.get("C:\\ws\\advent-of-code-2021\\src\\resources\\day24.txt"));
+//        Day23.solve(Paths.get("C:\\ws\\advent-of-code-2021\\src\\resources\\day23.txt"));
+        Day24.solve(Paths.get("C:\\ws\\advent-of-code-2021\\src\\resources\\day24.txt"));
 //        Day25.solve(Paths.get("C:\\ws\\advent-of-code-2021\\src\\resources\\day24.txt"));
     }
 }
---
+++ src/days/day24/Day24.kt
@@ -0,0 +1,48 @@
+package days.day24
+
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+
+object Day24 {
+
+    @JvmStatic
+    @Throws(IOException::class)
+    fun solve(input: Path?) {
+        val operations = Files.readAllLines(input).map { it.toString() }.chunked(18).map { it.toOperation() }
+
+        findModelNumbers(0,  operations, mutableMapOf()).let {
+            println(it.maxOrNull())
+            println(it.minOrNull())
+        }
+    }
+
+    // some sort of depth first search
+    private fun findModelNumbers(startZ: Long, operations: List<Operation>, cache: MutableMap<Pair<Long, Int>, List<String>>): List<String> {
+        val searchKey = Pair(startZ, operations.size)
+        if (!cache.containsKey(searchKey)) {
+            if (operations.isEmpty()) {
+                return if (startZ == 0L) listOf("") else listOf()
+            }
+            cache[searchKey] = LongRange(1, 9).flatMap {
+                val z = operations.first().performOperation(startZ, it)
+                findModelNumbers(z, operations.drop(1), cache).map { part -> "$it$part" }
+            }
+        }
+
+        return cache[searchKey]!!
+    }
+
+    private fun String.getOperand() = this.split("\\s+".toRegex())[2].toLong()
+
+    private fun List<String>.toOperation(): Operation = Operation(listOf(4, 5, 15).map { this[it].getOperand() })
+
+    // this is effectively what the 18-instruction operation does
+    private fun Operation.performOperation(z: Long, number: Long): Long = if (operands[1] + z % 26 != number) {
+        (z / operands[0]) * 26 + number + this.operands[2]
+    } else {
+        z / operands[0]
+    }
+
+    data class Operation(val operands: List<Long>)
+}
